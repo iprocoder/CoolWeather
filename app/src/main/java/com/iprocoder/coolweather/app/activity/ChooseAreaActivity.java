fd794c47a8cc8bc33164364198fec067ef6d2e7e
iprocoder <iprocoder@gmail.com>
2015-05-20 14:06:25 +0800

    加入显示天气信息的功能以及新增切换城市和手动更新天气的功能

--- app/src/main/java/com/iprocoder/coolweather/app/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/iprocoder/coolweather/app/activity/ChooseAreaActivity.java
@@ -2,8 +2,12 @@ package com.iprocoder.coolweather.app.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.*;
@@ -23,6 +27,8 @@ import java.util.List;
  * Created by iprocoder on 15-5-19.
  */
 public class ChooseAreaActivity extends Activity {
+
+    public final String TAG = this.getClass().getSimpleName();
 
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
@@ -60,9 +66,23 @@ public class ChooseAreaActivity extends Activity {
      */
     private int currentLevel;
 
+    /**
+     * 是否从 WeatherActivity 中跳转过来
+     */
+    private boolean isFromWeatherActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // 已经选择了城市且不是从 WeatherActivity 跳转过来，才会直接跳转到 WeatherActivity
+        if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
@@ -79,6 +99,13 @@ public class ChooseAreaActivity extends Activity {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY) {
+                    String countyCode = countyList.get(position).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+                    intent.putExtra("county_code", countyCode);
+                    Log.i(TAG, "start WeatherActivity");
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
@@ -227,6 +254,10 @@ public class ChooseAreaActivity extends Activity {
         } else if (currentLevel == LEVEL_CITY) {
             queryProvinces();
         } else {
+            if (isFromWeatherActivity) {
+                Intent intent = new Intent(this, WeatherActivity.class);
+                startActivity(intent);
+            }
             finish();
         }
     }
